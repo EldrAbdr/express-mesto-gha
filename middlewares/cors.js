ef65affe656ed2db4ffaba79a6e62977cfdf7bbb
eldrabdr <eldar0788@gmail.com>
2022-07-13 23:03:33 +0500

    fix: cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,24 +1,23 @@
 const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
   'https://mesto.el.front.nomoredomains.xyz',
   'http://mesto.el.front.nomoredomains.xyz',
-  'localhost:3000',
+  'http://localhost:3000',
+  'https://localhost:3000',
 ];
 
-module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
+module.exports = (req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
-  }
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
   }
   next();
 };
