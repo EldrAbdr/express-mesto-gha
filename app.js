52218d8e139748fa919acead27fae9e8f283cd13
eldrabdr <eldar0788@gmail.com>
2022-07-13 23:41:21 +0500

    fix: cors

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const cors = require('cors');
 const routes = require('./routes/index');
 const errorsHandler = require('./middlewares/errors-handler');
 const { errorLogger, requestLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
-//app.use(helmet());
+app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
 
 app.use(requestLogger);
 
